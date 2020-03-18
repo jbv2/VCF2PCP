d4d6badb8c67698814d2ff75eb0e897b35b8969f
Judith <judith.vballesteros@gmail.com>
2020-03-17 22:03:03 -0600

    Retagging groups of pop info file

--- mkmodules/mk-make-pop-info/make_popinfo.R
+++ mkmodules/mk-make-pop-info/make_popinfo.R
@@ -1,5 +1,6 @@
 ## load libraries
 library("dplyr")
+library("tidyr")
 
 ## Read args from command line
 args = commandArgs(trailingOnly=TRUE)
@@ -7,7 +8,7 @@ args = commandArgs(trailingOnly=TRUE)
 ## Uncomment For debugging only
 ## Comment for production mode only
 # args[1] <- "test/data/sample22.LD.maf_filtered.fam" ## %.fam file
-# args[2] <- "test/reference/samples.txt" ## $POPULATIONS file
+# args[2] <- "test/reference/tag_data.tsv" ## $POPULATIONS file
 # args[3] <- "test/data/sample22.LD.maf_filtered.autosomal.popinfo.txt" ## %.clust file
 
 ## read files
@@ -18,12 +19,14 @@ fam_data.df <- read.table(file = args[1],
 fam_columns_need.df <- fam_data.df %>% select(1, 2)
 
 tag_data.df <- read.table(file = args[2],
-                          header = F,
-                          sep = " ", stringsAsFactors = F)
+                          header = T,
+                          sep = "\t", stringsAsFactors = F) %>% separate(sample, c("TAG", "V1"), ":")
+
+tag_columns_need.df <- tag_data.df %>% select(2, 4)
 
 ## merge files
 merged.df <- left_join(x = fam_columns_need.df,
-                       y = tag_data.df,
+                       y = tag_columns_need.df,
                        by = c("V2" = "V1")) 
 colnames(merged.df) <- c("FAMID","ID","POP")
 
--- mkmodules/mk-make-pop-info/testmodule.sh
+++ mkmodules/mk-make-pop-info/testmodule.sh
@@ -3,20 +3,17 @@
 
 ###
 ## environment variable setting
-export SAMPLE_INFO="./test/reference/samples.txt"
-export K_VALUE="3"
+export TAG_FILE="./test/reference/tag_data.tsv"
 ###
 
 echo "[>..] test running this module with data in test/data"
 ## Remove old test results, if any; then create test/reults dir
 rm -rf test/results
 mkdir -p test/results
-cd test/data
-pwd
 echo "[>>.] results will be created in test/results"
 ## Execute runmk.sh, it will find the basic example in test/data
 ## Move results from test/data to test/results
 ## results files is *.popinfo.txt
 bash runmk.sh \
-&& mv *.popinfo.txt test/results/ \
+&& mv test/data/*.popinfo.txt test/results/ \
 && echo "[>>>] Module Test Successful"
